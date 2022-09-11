98df795917357af3293250a705604722de5ca82c
Johnexzy <obajohn75@gmail.com>
2022-09-11 13:51:36 +0100

    :art: (added styles for dashboard ui)

--- src/pages/Dashboard/DashboardLayout.vue
+++ src/pages/Dashboard/DashboardLayout.vue
@@ -1,21 +1,40 @@
 <template>
-  <q-layout view="lhh lpR lFf">
-    <div class="nav-bar">
-      <q-toolbar>
-        <q-toolbar-title class="q-ml-md">
-          <q-img
-            src="../../assets/logo.svg"
-            spinner-color="primary"
-            style="width: 71.5px; height: 39.85px"
-            spinner-size="82px"
+  <q-layout view="lhh lpR lfr">
+    <q-header class="bg-white text-black q-py-sm" height-hint="98">
+      <div class="nav-bar">
+        <q-toolbar>
+          <q-toolbar-title class="q-ml-md">
+            <q-img
+              src="../../assets/logo.svg"
+              v-if="false"
+              spinner-color="primary"
+              style="width: 71.5px; height: 39.85px"
+              spinner-size="82px"
+            />
+          </q-toolbar-title>
+          <q-btn
+            label="Donate"
+            color="primary"
+            no-caps
+            class="donate-btn q-mr-xs"
           />
-        </q-toolbar-title>
-      </q-toolbar>
-    </div>
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+        </q-toolbar>
+      </div>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" side="left" bordered>
       <side-bar></side-bar>
     </q-drawer>
-
+    <q-drawer v-model="rightDrawerOpen" side="right" bordered>
+      <div class="q-pl-md">
+        <h6>Recent Activity</h6>
+      </div>
+      <q-list>
+        <q-item class="cursor-pointer">
+          <q-item-section>Icon as avatar</q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -24,11 +43,24 @@
 
 <script>
 import SideBar from 'src/components/SideBar.vue';
-
+import { ref } from 'vue';
 export default {
   name: 'DashboardLayout',
   setup() {
-    return {};
+    const leftDrawerOpen = ref(true);
+    const rightDrawerOpen = ref(true);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value;
+      },
+    };
   },
   components: { SideBar },
 };
--- src/pages/Dashboard/Home.vue
+++ src/pages/Dashboard/Home.vue
@@ -2,10 +2,10 @@
   <q-page padding>
     <div class="container">
       <div class="row q-col-gutter-lg">
-        <div class="col-12 col-md-3 col-lg-3">
+        <div class="col-12 col-md-4 col-lg-4">
           <q-card class="stats-summary bg-black rounded-borders" flat>
             <q-card-section>
-              <div class="text-h6 text-grey-5">Donation</div>
+              <div class="text-h6 text-grey-5">Your Donations</div>
             </q-card-section>
             <q-card-section>
               <div class="text-h3 text-weight-bolder text-white">$0</div>
@@ -15,23 +15,30 @@
         <div class="col-12 col-md-4 col-lg-4">
           <q-card class="stats-summary bg-black rounded-borders" flat>
             <q-card-section>
-              <div class="text-h6 text-grey-5">Average Amount Donated</div>
+              <div class="text-h6 text-grey-5">Total Donations</div>
             </q-card-section>
             <q-card-section>
               <div class="text-h3 text-weight-bolder text-white">$0</div>
             </q-card-section>
           </q-card>
         </div>
-        <div class="col-12 col-md-3 col-lg-3">
-          <q-card class="stats-summary">
+        <div class="col-12 col-md-4 col-lg-4">
+          <q-card class="stats-summary bg-black rounded-borders" flat>
             <q-card-section>
-              <div class="text-h6">Our Changing Planet</div>
-              <div class="text-subtitle2">by John Doe</div>
+              <div class="text-h6 text-grey-5">Donation</div>
             </q-card-section>
             <q-card-section>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              <div class="text-h3 text-weight-bolder text-white">$0</div>
             </q-card-section>
           </q-card>
+        </div>
+      </div>
+      <div class="row items-center justify-evenly q-my-lg q-py-lg">
+        <div class="col-12 col-md-8 col-lg-7 q-mt-lg q-pt-lg">
+          <h3 class="text-center">Start Donating To Help The Masai People</h3>
+          <div class="text-center">
+            <q-btn color="primary" no-caps class="donate-btn" label="Donate" />
+          </div>
         </div>
       </div>
     </div>
